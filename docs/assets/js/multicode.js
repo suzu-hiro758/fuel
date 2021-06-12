b0f620c50b75da7e7e79fec9bc94c2521268c500
suzuhiro <felix1809@gmail.com>
2021-06-13 00:27:08 +0900

    html form

---
+++ docs/assets/js/multicode.js
@@ -0,0 +1,21 @@
+window.addEventListener('DOMContentLoaded', () => {
+  document.querySelectorAll('.code.multi').forEach(item => {
+    const radios = item.querySelectorAll('[type="radio"]')
+    radios.forEach(radio => {
+      radio.addEventListener('change', evt => {
+        const active = evt.target.value;
+        item.querySelectorAll('code').forEach(code => {
+          code.style.display = active === code.id ? 'block' : 'none';
+        })
+        radios.forEach(radio => {
+          if (radio.checked) {
+            radio.parentNode.classList.add('active');
+          } else {
+            radio.parentNode.classList.remove('active');
+          }
+        })
+      })
+    })
+    radios.item(0).click();
+  })
+});
