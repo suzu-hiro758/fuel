5a88ed89cffa0fa957808de892fe8146be44ef32
suzuhiro <felix1809@gmail.com>
2022-03-17 23:50:58 +0900

    web component

---
+++ docs/d419e799b/html/module/question-one.js
@@ -0,0 +1,27 @@
+const template = document.createElement("template");
+template.innerHTML = `
+<style>
+  .some-class {
+    font-family: Arial;
+    font-size: 14px;
+    color: blue;
+  }
+</style>
+<div>
+  This is some text
+</div>
+<div class="some-class">
+  And here's some more text
+</div>
+`;
+
+class QuestionOne extends HTMLElement {
+  connectedCallback() {
+    if (!this.shadowRoot) {
+      this.attachShadow({ mode: "open" });
+      this.shadowRoot.appendChild(template.content.cloneNode(true));
+    }
+  }
+}
+
+customElements.define("question-one", QuestionOne);
