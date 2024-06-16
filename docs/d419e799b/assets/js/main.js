02ef197a2ec327325fb0ebf82a276911cdc3f3dd
suzuhiro <felix1809@gmail.com>
2024-06-16 22:23:01 +0900

    web component リファクタ

--- docs/d419e799b/assets/js/main.js
+++ docs/d419e799b/assets/js/main.js
@@ -1,6 +1,9 @@
-const source = {};
 
 class CodeText extends HTMLElement {
+
+  constructor() {
+    super()
+  }
 
   connectedCallback() {
     const wrapper = document.createElement('code');
@@ -8,11 +11,11 @@ class CodeText extends HTMLElement {
     const type = this.getAttribute('codetype');
     wrapper.className = `language-${type}`;
     wrapper.id = id;
-    wrapper.textContent = source[id];
+
+    wrapper.textContent = document.getElementById(id).innerHTML.trim()
     this.parentElement.appendChild(wrapper);
     this.parentElement.removeChild(this);
   }
-
 }
 
 customElements.define( 'code-text', CodeText );
